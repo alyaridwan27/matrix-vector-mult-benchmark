b1e62a5d4714323e3a368d00e50b60d4b7df0177
alyaridwan27 <114371692+alyaridwan27@users.noreply.github.com>
2022-12-13 19:10:06 +0700

    Update main_b_space.c

--- main_b_space.c
+++ main_b_space.c
@@ -1,24 +1,46 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 #include "mylib/mylib.h"
 
-int main(){
+#define LOWER 1
+#define UPPER 9
 
-    int N;
-    printf("enter N:\n");
-    scanf("%d", &N);
+int generateRandomInt(int lower, int upper){
+    return (rand() % (upper-lower+1)) + lower;
+}
 
-    int matrix[N*N];
-    int vector[N];
-    int result[N];
-
-    initMat(matrix, N);
-    initVec(vector, N);
-
-    while(1){
-        matVecMult(matrix, vector, result, N);
+void initMat(int* mat, int N){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            mat[N*i+j] = generateRandomInt(LOWER,UPPER);
+        }
     }
+}
+
+void initVec(int* vec, int N){
+    for(int i=0; i<N; i++){
+        vec[i] = generateRandomInt(LOWER,UPPER);
+    }
+}
+
+int main(void) {
+
+    int N = 1500;
+    
+    int* mat = (int *) malloc(N*N*sizeof(int));
+    int* vec = (int *) malloc(N*sizeof(int));
+    int* vecResult = (int *) malloc(N*sizeof(int));
+
+    initMat(mat, N);
+    initVec(vec, N);
+
+    while (1) {
+        matVecMult(mat, vec, vecResult, N);
+    }
+
     return 0;
+
+    
 }
