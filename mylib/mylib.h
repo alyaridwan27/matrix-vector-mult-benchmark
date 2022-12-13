928a343cdb7637ef7fc367838004a426702a2e8b
alyaridwan27 <114371692+alyaridwan27@users.noreply.github.com>
2022-12-13 19:13:13 +0700

    Update mylib.h

--- mylib/mylib.h
+++ mylib/mylib.h
@@ -1,6 +1,7 @@
-void printMat(int* M, int N);
-void printVec(int* v, int N);
-int* matVecMult(int* M, int* v, int *result, int N);
 int generateRandomInt(int lower, int upper);
-void initMat(int* M, int N);
-void initVec(int* v, int N);
+void initMat(int* mat, int N);
+void initVec(int* vec, int N);
+void printMat(int* mat, int N);
+void printVec(int* vec, int N);
+void matVecMult(int* mat, int* vec, int* vecResult, int N);
+void multiply_vector_matrix(int *mat, int *vec, int N, int *vector_expected);
